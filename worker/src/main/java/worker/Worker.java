137d6b8dcd3b848cddbd9ab6a076d569e34ca3ad
aagarwa <aagarwa@adobe.com>
2019-08-08 11:51:21 +0530

    dummy change

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 
 class Worker {
   public static void main(String[] args) {
+    
     try {
       Jedis redis = connectToRedis("redis");
       Connection dbConn = connectToDB("db");
